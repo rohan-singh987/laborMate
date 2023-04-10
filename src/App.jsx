84ec5d8064b064138b5f3048c531a530e242edee
Rohan Singh <rohannsingh98765@gmail.com>
2023-04-10 15:06:31 +0530

    roting
    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import LaborCards from './components/LaborCards'
 import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import NewEmployee from './components/NewEmployee'
 
 const App = () => {
   return (
     <div >
+
       <Navbar />
+      <Routes>
+        <Route path='/' element={<LaborCards />} />
+        <Route path='/newemployee' element={ <NewEmployee /> } />
+      </Routes>
     </div>
   )
 }
