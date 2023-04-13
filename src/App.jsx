1faa8964f108b22519af7ab6ac20503dbf197a5c
Rohan Singh <rohannsingh98765@gmail.com>
2023-04-13 06:26:09 +0530

    labor card
    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
-import React from 'react'
+import { createContext, useState, useEffect } from 'react'
 import LaborCards from './components/LaborCards'
 import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import NewEmployee from './components/NewEmployee'
+import Error from './pages/Error'
+import Login from './pages/Login';
+import Signup from './pages/Signup'
+
+const Appstate = createContext();
+
 
 const App = () => {
-  return (
-    <div className='relative' >
 
+  const [login, setLogin] = useState(false);
+  const [userName, setUserName] = useState("")
+
+  return (
+
+    <Appstate.Provider value={{login, userName, setLogin, setUserName }} >
+
+    <div className='relative' >
       <Navbar />
       <Routes>
         <Route path='/' element={<LaborCards />} />
         <Route path='/newemployee' element={ <NewEmployee /> } />
+        <Route />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='*' element={<Error />} />
       </Routes>
     </div>
+
+    </Appstate.Provider>
   )
 }
 
-export default App
+export default App;
+export {Appstate};
--- src/components/LaborCards.jsx
+++ src/components/LaborCards.jsx
@@ -1,61 +1,73 @@
+import { getDocs } from 'firebase/firestore';
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { Circles } from 'react-loader-spinner';
 import ReactStars from 'react-stars';
+import { employeeRef } from '../firebase/firebase';
 
 const LaborCards = () => {
 
-  const [detail, setDetail] = useState([
-    {
-      img: 'https://images.unsplash.com/photo-1537511446984-935f663eb1f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-      name: "Ramesh",
-      post: 'Worker',
-      charges: '400',
-      exp: '4',
-      rating: '4',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1537511446984-935f663eb1f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-      name: "Ramesh",
-      post: 'Worker',
-      charges: '400',
-      exp: '4',
-      rating: '4',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1537511446984-935f663eb1f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
-      name: "Ramesh",
-      post: 'Worker',
-      charges: '400',
-      exp: '4',
-      rating: '4',
-    },
-  ]);
+  const [detail, setDetail] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    const getData = async () => {
+        setLoading(true);
+
+        const _data = await getDocs(employeeRef);
+        _data.forEach((doc) => {
+          setDetail((prevData) => [...prevData, {...(doc.data()), id:doc.id}])
+        })
+
+        setLoading(false);
+    }
+
+    getData()
+  }, [])
+
 
   return (
 
-    <div className='flex flex-wrap justify-between p-3 mt-2'>
-      {detail.map((element, index) => {
-        return (
-          <div key={index} className='m-5 bg-gradient-to-r from-[#8785A2] via-[#DBE2EF] to-[#AEE1E1] truncate shadow-lg text-white cursor-pointer text-lg font-medium hover:scale-110 transition delay-5 duration-500 ease-in-out' >
-            <img src="https://images.unsplash.com/photo-1537511446984-935f663eb1f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="worker" className='m-0 w-72 m-1' />
+    <div className='flex flex-wrap justify-between p-3  bg-gradient-to-r from-[#cebff3] via-[#c4cdd9e8] to-[#ebf5b3de] '>
+      { loading ? <div className='w-full flex justify-center items-center'> <Circles width={80} color="black" /> </div> :
 
+        detail.map((element, index) => {
+          return (
+            <div key={index} className='m-5 bg-gradient-to-r from-[#8785A2] via-[#DBE2EF] to-[#AEE1E1] truncate shadow-lg text-white cursor-pointer text-lg font-medium hover:scale-110 transition delay-5 duration-500 ease-in-out' >
+            <img src={element.img } alt="worker" className='m-0 w-72 m-1' />
+            
             <div className='flex flex-col justify-center content-center items-center text-black'>
-              <h1>Name: <span className='text-[#3282B8]  no-underline hover:underline font-bold'>{element.name}</span> </h1>
-              <h1>Post: <span className='text-[#3282B8]  no-underline hover:underline font-bold'>{element.post}</span></h1>
-              <h1>Visiting Charges : <span className='text-[#3282B8]  no-underline hover:underline font-bold'>{element.charges}</span></h1>
-              <h1>Experience: <span className='text-[#3282B8]  no-underline hover:underline font-bold'>{element.exp}</span> </h1>
-              <h1 className='flex bg-[#769FCD] px-2 my-3 items-center'>Rating: 
-                <ReactStars 
-                  size={20}
-                  half={true}
-                  value={element.rating}
-                  edit={false}
-                  className="ml-2 "
-                /></h1>
+            <h1>Name:
+            <span className='text-[#3282B8]  no-underline hover:underline font-bold'>{element.name}</span>
+            </h1>
+            
+            <h1>Post:
+            <span className='text-[#3282B8]  no-underline hover:underline font-bold'>{element.post}</span>
+            </h1>
+            
+            <h1>Visiting Charges :
+            <span className='text-[#3282B8]  no-underline hover:underline font-bold'>{element.charges}</span>
+            </h1>
+            
+            <h1>Experience:
+            <span className='text-[#3282B8]  no-underline hover:underline font-bold'>{element.exp}</span>
+            </h1>
+            
+            
+            <h1 className='flex bg-[#769FCD] px-2 my-3 items-center'>Rating:
+            <ReactStars
+            size={20}
+            half={true}
+            value={5}
+            edit={false}
+            className="ml-2 " />
+            </h1>
             </div>
-          </div>
-        )
-      })}
+            </div>
+            )
+          })
+      }
     </div>
   )
 }
